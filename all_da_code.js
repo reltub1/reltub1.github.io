10e2a2199db061d6c3219c24434624211cc2a17a
SuperCow12653 <>
2025-05-29 17:26:37 -0400

    mas
    m

---
+++ all_da_code.js
@@ -0,0 +1,21 @@
+giriffraff = document.getElementById("8")
+
+
+
+
+
+
+
+
+
+
+function zebras(){
+    console.log("mo money mo problems  ")
+    document.getElementById("8").textContent = "idk how 2 do dat";
+
+
+
+}
+
+
+
